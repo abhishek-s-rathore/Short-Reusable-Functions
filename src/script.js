81117761e2d850e3593a4c606d080be2089ff934
Abhishek Singh Rathore <abhishek.990914@gmail.com>
2024-01-10 19:06:55 +0530

    Create An Array Of Numbers From 1 to n with step s

--- src/script.js
+++ src/script.js
@@ -8,6 +8,7 @@ function* indexGenerator(){
 const index = indexGenerator();
 console.log(index.next().value);
 
+
 // Calculate Time Of Execution
 (function CalculateTime(){
 console.time('calculated_time');
@@ -16,14 +17,26 @@ console.timeEnd('calculated_time');
 })();
 
 
-// Create An Array Of Numbers From 1 to n
-
+// Create An Array Of Numbers From "1" to "n"
 let range = (n)=>{
- return Array.from({length: n}, (_, i)=>i+1)
-}
+ return Array.from({length: n}, (_, i)=>i+1);
+};
+console.log(numbers(5));
+
+
+// Create An Array Of Numbers From "1" to "n" with step "s"
+let numbers = (n,step=1, skipZero=false)=>{
+   return Array.from({length: n}, (_, i)=> skipZero ? (i===0 ? 1 :i*step+1) : i*step);
+  };
+console.log(numbers(6,5,true));
+
 
 // Create An Array Of Length "n" and Fill With Value "f"
-
 let fill = (n,f)=>{
-  return Array(n).fill(f)
- }
+  return Array(n).fill(f);
+ };
+console.log(fill(10,4));
+
+
+ 
+ 
