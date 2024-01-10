96c5f19f52bd3c2b35af467cb181a8ca4c87d32e
Abhishek Singh Rathore <abhishek.990914@gmail.com>
2024-01-10 18:35:34 +0530

    Create An Array Of Length n and Fill With Value f

--- src/script.js
+++ src/script.js
@@ -18,6 +18,12 @@ console.timeEnd('calculated_time');
 
 // Create An Array Of Numbers From 1 to n
 
-let numbers = (n)=>{
-  Array.from({length: n}, (_, i)=>i+1)
+let range = (n)=>{
+ return Array.from({length: n}, (_, i)=>i+1)
 }
+
+// Create An Array Of Length "n" and Fill With Value "f"
+
+let fill = (n,f)=>{
+  return Array(n).fill(f)
+ }
